a11c44ed01e54b2af5aa756e120bc5d255af13e6
Data Dius <16731794+datadius@users.noreply.github.com>
2024-02-25 18:50:15 +0100

    added eprintln macro to print errors to terminal

--- lightgrep/src/main.rs
+++ lightgrep/src/main.rs
@@ -6,10 +6,12 @@ use lightgrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = lightgrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
