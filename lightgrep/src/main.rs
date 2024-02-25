bb997c44ad17db7a3a368f4b12d97fed19304a1f
Data Dius <16731794+datadius@users.noreply.github.com>
2024-02-25 18:06:56 +0100

    reorganize with lib.rs

--- lightgrep/src/main.rs
+++ lightgrep/src/main.rs
@@ -1,11 +1,12 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use lightgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -13,25 +14,9 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    if let Err(e) = lightgrep::run(config) {
+        println!("Application error: {}", e);
 
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+        process::exit(1);
     }
 }
